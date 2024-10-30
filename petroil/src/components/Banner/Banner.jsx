e24c511222c42f0563105e395ece634d92d5034e
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-10-30 20:35:30 +0600

    Added Banner

---
+++ petroil/src/components/Banner/Banner.jsx
@@ -0,0 +1,14 @@
+import banner from '../../assets/banner.png'
+
+export default function Banner() {
+    return (
+      <div style={{backgroundImage:`url(${banner})`}} className=" bg-cover bg-no-repeat w-full py-[257px] relative">
+        <div className="absolute inset-0 bg-[#00000099]" />
+        <div className='max-w-[1140px] mx-auto text-left relative z-10'>
+            <h1 className='max-w-[842px] text-white font-bold text-7xl mb-7'>We exist since 1975 on the oil and gas industry.</h1>
+            <button className='bg-[#F40404] py-[13px] px-10 text-white font-semibold'>LEARN MORE</button>
+        </div>
+      </div>
+    )
+  }
+  
