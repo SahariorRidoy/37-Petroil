19a48dbe2c2c6ad272ba1ff7b915ba297e8de5ac
MD. Saharior Ridoy <emonhridoy2014@gmail.com>
2024-11-02 15:29:32 +0600

    Added button color and deploy to surge

--- petroil/src/components/Map/Map.jsx
+++ petroil/src/components/Map/Map.jsx
@@ -7,7 +7,7 @@ const Map = () => {
       <div className="bg-[#F40404] py-12 border-b-4 border-b-[#FFB800]">
         <div className="max-w-[1140px] mx-auto flex justify-between items-center ">
           <h2 className="font-bold text-4xl text-white">Want to join as member branch in your area?</h2>
-            <button className="border-2 border-white text-white py-3 px-8 font-semibold text-center">Contact</button>
+            <button className="border-2 border-white text-white py-3 px-8 font-semibold text-center hover:bg-white hover:text-[#F40404]">Contact</button>
         </div>
       </div>
     </div>
